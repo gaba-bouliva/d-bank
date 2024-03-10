44a3119d3fd399bc8a7d2db214798aeadaa4fbf9
eagle-bald <eagle@sky.com>
2024-03-10 01:04:27 +0100

    Setup database and basic CRUD

---
+++ db/sqlc/store.go
@@ -0,0 +1,152 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store provides all functions to execute db queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+var txKey = struct{}{}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes a function within a database transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("db tx err: %v %v", err, rbErr)
+		}
+
+		return err
+	}
+
+	return tx.Commit()
+}
+
+//TranferTxParams contains the input parameters of the tranfer transaction
+type TransferTxParams struct {
+	FromAccountID	int64 `json:"from_account_id"`
+	ToAccountID 	int64 `json:"to_account_id"`
+	Amount 			int64 `json:"amount"`
+}
+
+// TransferTxResult is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer 		Transfer `json:"transfer"`
+	FromAccount	Account	 `json:"from_account"`
+	ToAccount		Account  `json:"to_account"`
+	FromEntry 	Entry 	 `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TranferTx performs a money transfer from one account to the other.
+// It creates a tranfer record, add account entries, and update accountss balance witthin a single db transaction
+func (store *Store)TranferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error){
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID: 	 arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Update accounts' balances
+		if arg.FromAccountID  < arg.ToAccountID {
+			result.FromAccount, result.ToAccount , err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+			if err != nil {
+				return err
+			}
+
+		} else {
+			
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+			if err != nil {
+				return err
+			}
+		}
+		
+
+
+		return nil
+	})
+
+	return result, err 
+}
+
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64) (account1 Account, account2 Account, err error) {
+		 err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
+			ID: accountID1,
+			Amount: amount1,
+		})
+		if err != nil {
+			return 
+		}
+
+		account1, err = q.GetAccount(ctx, accountID1)
+			if err != nil {
+				return
+			}
+
+
+			err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
+				ID: accountID2,
+				Amount: amount2,
+			})
+			if err != nil {
+				return 
+			}
+	
+			account2, err = q.GetAccount(ctx, accountID2)
+				if err != nil {
+					return
+				}
+
+			return
+	} 
